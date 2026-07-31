3f223653c621cec16426a4354490b300e64f72bc
hamawakiac <hamawakiac@gmail.com>
2026-07-31 21:29:57 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,16 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyD88NeTFnbp3PYmHaTyF55BbTgK3QGKTjU",
-  authDomain: "hamawaki-ac-menu.firebaseapp.com",
-  projectId: "hamawaki-ac-menu",
-  storageBucket: "hamawaki-ac-menu.firebasestorage.app",
-  messagingSenderId: "754881407382",
-  appId: "1:754881407382:web:75f8eb5d95b33ce68095b7"
+  apiKey: "AIzaSyC5bkTxBQ-tx6nQtGuM0SbQm0WU6d-NQ1s",
+  authDomain: "hamawakiac-menu.firebaseapp.com",
+  projectId: "hamawakiac-menu",
+  storageBucket: "hamawakiac-menu.firebasestorage.app",
+  messagingSenderId: "990908166780",
+  appId: "1:990908166780:web:3867dc5695ec7548d30ff9",
+  measurementId: "G-QSN0E08E9Y"
+};
+
+
+// ログインIDとFirebase Authenticationのメールアドレスの対応表
+// 左側がログイン画面で入力するID、右側がAuthenticationに登録したメールです。
+window.LOGIN_ID_MAP = {
+  "hamawakiac": "hamawakiac@gmail.com"
 };
